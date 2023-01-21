18474e83025dd7fca3576611c7eb8b62c0dbf10a
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-01-21 08:02:54 +0000

    refactor html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style.css';
 
-const tasks = require('./tasks.js');
+let tasks = [];
 // array of objects
 const taskList = document.getElementById('task-list'); // ul dom element
 
 // function that fill the Todo List
-const fillList = (tasks) => {
+export const fillList = (tasks) => {
   let taskList = '';
   tasks.forEach((task) => {
     const taskItem = `<li id="todo-item"><input type="checkbox">${task.description}</li>`;
@@ -19,3 +19,5 @@ window.onload = () => {
   tasks.sort((a, b) => a.index - b.index);
   taskList.innerHTML = fillList(tasks);
 };
+
+
