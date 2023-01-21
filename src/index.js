e56ba45ad80ebe2535b824009333dedbaaf061dd
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-01-21 03:28:49 +0000

    created an array of objects ie taks

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import _ from 'lodash';
 import printMe from './print.js';
 import './style.css';
 
+const tasks = require ('./task.js')
---
+++ src/taks.js
@@ -0,0 +1,24 @@
+const tasks = [
+  {
+      description: "Take a Shwoer",
+      completed: false,
+      index: 1
+  },
+  {
+      description: "Cook today",
+      completed: false,
+      index: 2
+  },
+  {
+      description: "Buy groceries",
+      completed: false,
+      index: 3
+  },
+  {
+      description: "Microverse Homework",
+      completed: false,
+      index: 4
+  }
+];
+
+module.exports = tasks;
